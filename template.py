144affa53f56c80ab02e9edf66522b222851c74e
Fredrik Karlsson <fredrik.a.karlsson@gmail.com>
2016-01-16 15:57:26 +0100

    Added problem template

---
+++ template.py
@@ -0,0 +1,13 @@
+'''
+PROBLEM (X): 
+Describe probleme here
+
+ANSWER : Fill in answer when solved
+'''
+
+def main():
+    # Code here
+    print "Template"
+
+if __name__ == "__main__":
+    main()
