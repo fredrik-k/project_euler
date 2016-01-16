fc42f7bd6a6eb51c32df7900ab91d24fb60bbeeb
Fredrik Karlsson <fredrik.a.karlsson@gmail.com>
2016-01-16 18:03:22 +0100

    solution to problem 3

---
+++ p_1_10/p3.py
@@ -0,0 +1,34 @@
+'''
+PROBLEM 3: 
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143?
+
+ANSWER : 6857
+'''
+
+from math import floor
+
+def findDivisor(n) :
+	upperLimit = n**0.5
+	for i in range(2, int(floor(upperLimit))) :
+		if n % i == 0 : 
+			return i
+
+def findPrimeFactors(n, primeList = []) :
+	divisor = findDivisor(n)
+	if (divisor > 0) : 
+		primeList = findPrimeFactors(divisor, primeList)
+		primeList = findPrimeFactors(n / divisor, primeList)
+	else :
+		primeList.append(n)
+	return primeList
+
+def main():
+    primeList = findPrimeFactors(600851475143)
+
+    primeList.sort(reverse=True)
+    print "Largest prime factor of 600851475143 is " , primeList[0]
+
+if __name__ == "__main__":
+    main()
