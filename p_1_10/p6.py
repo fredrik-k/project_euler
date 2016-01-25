a2a44a2eb8fdb0ebd8d542f6728e5e43193723ec
Fredrik Karlsson <fredrik.a.karlsson@gmail.com>
2016-01-25 19:30:49 +0100

    Fixed trailing white spaces in problem 6

--- p_1_10/p6.py
+++ p_1_10/p6.py
@@ -13,7 +13,7 @@ numbers and the square of the sum is 3025 - 385 = 2640.
 Find the difference between the sum of the squares of the first one hundred
 natural numbers and the square of the sum.
 
-ANSWER: 
+ANSWER: 25164150
 '''
 
 def main() :
